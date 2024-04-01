9eee8b998261a379b35f51efee636874386d9feb
YavuzKarapinar <yavuzkrpnr01@gmail.com>
2024-04-01 13:46:48 +0300

    init

---
+++ src/main/java/me/jazzy/obp/OnlineBlogPortalApplication.java
@@ -0,0 +1,13 @@
+package me.jazzy.obp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBlogPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBlogPortalApplication.class, args);
+    }
+
+}
