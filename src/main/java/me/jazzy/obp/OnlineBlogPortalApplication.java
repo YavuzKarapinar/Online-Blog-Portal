6d70675ad5079e4bf4aa914965e1e2314299f30e
YavuzKarapinar <yavuzkrpnr01@gmail.com>
2024-04-03 17:04:21 +0300

    debug: Swagger Authorization bug fixed

--- src/main/java/me/jazzy/obp/OnlineBlogPortalApplication.java
+++ src/main/java/me/jazzy/obp/OnlineBlogPortalApplication.java
@@ -1,9 +1,13 @@
 package me.jazzy.obp;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityScheme(name = "obp", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class OnlineBlogPortalApplication {
 
     public static void main(String[] args) {
