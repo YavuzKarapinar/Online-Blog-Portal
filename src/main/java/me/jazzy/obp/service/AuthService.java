5272a19ec75f0fd046c9024a86e79dc95840cb8e
YavuzKarapinar <yavuzkrpnr01@gmail.com>
2024-04-22 20:05:30 +0300

    debug: unused import removed

--- src/main/java/me/jazzy/obp/service/AuthService.java
+++ src/main/java/me/jazzy/obp/service/AuthService.java
@@ -14,7 +14,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
